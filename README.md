721d7ead4995b308dbe37131b672a0608b5954be
errazur <systemhorscombat@gmail.com>
2023-06-20 14:48:07 +0200

    initial commit with error corrected

---
+++ README.md
@@ -0,0 +1,15 @@
+# online-voting
+Secure Online Voting System
+> Pre-requsites
+* PhP
+* XAMPP or WAMPP Server to run locally
+* PhP mailer and composer
+* SMTP Account if you want to send vote confirmation e-mail
+
+# What all to be done
+* Put your smtp credentials in required files([approve.php](approve.php),[voted-detail.php](voted-detail.php),[register.php](register.php))
+* Open phpmyadmin
+* Import [dbms.sql](dbms.sql) by creating a new database: dbms
+* Enter a new password in the admin table in order to access admin area
+* Use this password in the admin.php file to successfully login
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.8.0',
+            'version' => '6.8.0.0',
+            'reference' => 'df16b615e371d81fb79e506277faea67a1be18f1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpstan/phpstan' => array(
+            'pretty_version' => '1.10.19',
+            'version' => '1.10.19.0',
+            'reference' => 'af5a296ff02610c1bfb4ddfac9fd4a08657b9046',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpstan/phpstan',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
